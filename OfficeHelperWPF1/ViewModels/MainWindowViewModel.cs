3bca87aadfdd05425e249b1e584b3424b6bed07e
Naeel <nailkhakimov123@mail.ru>
2025-07-09 10:13:06 +0500

    Небольшой тест изображения листа в датагриде

--- OfficeHelperWPF1/ViewModels/MainWindowViewModel.cs
+++ OfficeHelperWPF1/ViewModels/MainWindowViewModel.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OfficeHelperWPF1.Models;
 using OfficeHelperWPF1.ViewModels.Base;
 
 namespace OfficeHelperWPF1.ViewModels
 {
     class MainWindowViewModel : ViewModel
     {
-
+        public List<OfficeEquipment> OfficeEquipment
+        {
+            get;
+            set;
+        } = new List<OfficeEquipment> { new() { Id = 1, Name = "some", Status = "s", Type = "t" }, new() { Id = 2, Name = "222", Status = "s2", Type = "t2" } };
     }
 }
